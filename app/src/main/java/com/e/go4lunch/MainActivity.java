81961615654528354abdfedffa63fb8cd1d8f86f
loots <ludotracks@hotmail.com>
2020-02-28 09:57:29 +0100

    First screen  add button xml

--- app/src/main/java/com/e/go4lunch/MainActivity.java
+++ app/src/main/java/com/e/go4lunch/MainActivity.java
@@ -4,11 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.firebase.ui.auth.AuthUI;
 
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RC_SIGN_IN = 123;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       // startSignInActivity();
+    }
+
+    // ------------------
+    // NAVIGATION
+    // ------------------
+
+    private void startSignInActivity() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.AppTheme)
+                        .setAvailableProviders(
+                                Arrays.asList(
+                                        new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(), //GOOGLE
+                                        new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build())) // FACEBOOK
+
+                        .build(),
+                RC_SIGN_IN);
     }
 }
