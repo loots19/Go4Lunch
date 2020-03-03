15a168fee3c13584de9fe22314ab05e26eeef4c5
loots <ludotracks@hotmail.com>
2020-03-03 08:57:51 +0100

    add method for current location

--- app/src/main/java/com/e/go4lunch/MainActivity.java
+++ app/src/main/java/com/e/go4lunch/MainActivity.java
@@ -1,13 +1,27 @@
 package com.e.go4lunch;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -17,45 +31,59 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
     //FOR DESIGN
     @BindView(R.id.btnGG)
     Button buttonloginGg;
     @BindView(R.id.btnFb)
     Button buttonFb;
 
+
+
     // --------------------
     // UTILS
     // --------------------
 
     @Nullable
-    protected FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+    protected FirebaseUser getCurrentUser() {
+        return FirebaseAuth.getInstance().getCurrentUser();
+    }
 
-    protected Boolean isCurrentUserLogged(){ return (this.getCurrentUser() != null); }
+    protected Boolean isCurrentUserLogged() {
+        return (this.getCurrentUser() != null);
+    }
+
     //FOR DATA
+
     private static final int RC_SIGN_IN = 123;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this); //Configure Butterknife
 
+
+
     }
+
+
     // --------------------
     // ACTIONS
     // --------------------
     @OnClick(R.id.btnGG)
-    public void loginGg(){
-    // Launch Sign-In Activity when user clicked on Login Button
+    public void loginGg() {
+        // Launch Sign-In Activity when user clicked on Login Button
         if (this.isCurrentUserLogged()) {
-        this.startMapsActivity();
-    } else {
+            this.startMapsActivity();
+        } else {
             this.startSignInActivity();
         }
     }
 
     @OnClick(R.id.btnFb)
-    public void loginFb(){
+    public void loginFb() {
         // Launch Sign-In Activity when user clicked on Login Button
         if (this.isCurrentUserLogged()) {
             this.startMapsActivity();
@@ -66,7 +94,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void startMapsActivity() {
-        Intent intent = new Intent(this,MapsActivity.class);
+        Intent intent = new Intent(this, MapsActivity.class);
         startActivity(intent);
     }
     // ------------------
@@ -84,6 +112,7 @@ public class MainActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
     }
+
     private void startSignInActivityFb() {
         startActivityForResult(
                 AuthUI.getInstance()
@@ -95,4 +124,7 @@ public class MainActivity extends AppCompatActivity {
                         .build(),
                 RC_SIGN_IN);
     }
+
+
+
 }
--- app/src/main/java/com/e/go4lunch/MapsActivity.java
+++ app/src/main/java/com/e/go4lunch/MapsActivity.java
@@ -1,28 +1,71 @@
 package com.e.go4lunch;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.Api;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+import java.io.FileDescriptor;
+import java.io.PrintWriter;
+import java.util.concurrent.TimeUnit;
+
+public class MapsActivity extends FragmentActivity implements
+    OnMapReadyCallback,
+    GoogleApiClient.ConnectionCallbacks,
+    GoogleApiClient.OnConnectionFailedListener,
+    LocationListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoolgeApiClient;
+    private LocationRequest mLocationRequest;
+    private Location lastLocation;
+    private Marker currentUserLocationMarker;
+    private static final int REQUEST_USER_LOCATION_CODE = 99;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            checkUserLocationPermission();
+        }
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -37,11 +80,111 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng Bouffemont = new LatLng(49.041200, 2.298334);
-        mMap.addMarker(new MarkerOptions().position(Bouffemont).title("Marker in Bouffemont"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(Bouffemont));
+        mMap = googleMap;
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            builGoogleApiClent();
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+    public boolean checkUserLocationPermission(){
+
+     if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+         if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
+             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_USER_LOCATION_CODE);
+         }
+         else{
+             ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_USER_LOCATION_CODE);
+
+         }
+         return false;
+     }
+     else{
+         return true;
+     }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        switch (requestCode){
+            case REQUEST_USER_LOCATION_CODE:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+                      if(mGoolgeApiClient == null){
+                         builGoogleApiClent();
+                      }
+                      mMap.setMyLocationEnabled(true);
+
+                    }
+                }
+                else {
+                    Toast.makeText(this,"Permission denied...",Toast.LENGTH_LONG).show();
+                }
+        }
+    }
+
+    protected synchronized void builGoogleApiClent() {
+        mGoolgeApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mGoolgeApiClient.connect();
+
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1100);
+        mLocationRequest.setFastestInterval(1100);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoolgeApiClient,mLocationRequest,this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+       lastLocation = location;
+
+       if (currentUserLocationMarker != null) {
+           currentUserLocationMarker.remove();
+       }
+       LatLng latLng = new LatLng(location.getAltitude(),location.getLongitude());
+
+       MarkerOptions markerOptions = new MarkerOptions();
+       markerOptions.position(latLng);
+       markerOptions.title("vous êtes ici");
+       markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+       currentUserLocationMarker = mMap.addMarker(markerOptions);
+
+       mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+       mMap.animateCamera(CameraUpdateFactory.zoomBy(15));
+
+       if(mGoolgeApiClient != null){
+           LocationServices.FusedLocationApi.removeLocationUpdates(mGoolgeApiClient,this);
+       }
     }
 }
