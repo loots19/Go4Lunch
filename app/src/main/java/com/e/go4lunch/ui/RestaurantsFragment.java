e66343112ef72d716124d4a77d8b101b3d15cab0
loots <ludotracks@hotmail.com>
2020-04-14 10:36:16 +0200

    just make model class

--- app/src/main/java/com/e/go4lunch/ui/RestaurantsFragment.java
+++ app/src/main/java/com/e/go4lunch/ui/RestaurantsFragment.java
@@ -3,14 +3,12 @@ package com.e.go4lunch.ui;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -21,9 +19,6 @@ import com.e.go4lunch.R;
 import com.e.go4lunch.adapter.RestaurantAdapter;
 import com.e.go4lunch.models.myPlace.MyPlace;
 import com.e.go4lunch.models.myPlace.Result;
-import com.e.go4lunch.models.placeDetail.PlaceDetail;
-import com.e.go4lunch.models.placeDetail.ResultDetail;
-import com.e.go4lunch.viewmodels.RestaurantDetailViewModel;
 import com.e.go4lunch.viewmodels.RestaurantViewModel;
 import com.google.gson.Gson;
 
